3692d7a791ba8c1b1c7a0cf535c8d2475a04a88c
Mutiu Adegboye <adegboyemutiu@gmail.com>
2024-08-22 16:04:27 +0100

    add project structure

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'GenueChat',
+    version= '0.0.0',
+    author= 'Mutiu Adegboye',
+    author_email= 'adegboyemutiu@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
