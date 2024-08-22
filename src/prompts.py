e1918301aee7086bdb574d13b9fc36a8d1f7f05b
Mutiu Adegboye <adegboyemutiu@gmail.com>
2024-08-22 17:04:00 +0100

    add prompts file

---
+++ src/prompts.py
@@ -0,0 +1,12 @@
+template = """You are a helpful assistant.
+Current conversation:
+{history}
+Human: {input}
+AI Assistant:"""
+
+
+document_template = """Answer the question based only on the following context:
+{context}
+
+Question: {question}
+"""
